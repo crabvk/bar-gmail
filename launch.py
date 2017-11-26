26ee11717da050add9abc3b3bebe2b6fb5752bf4
iwconfig <snelhingst@gmail.com>
2017-11-26 20:05:40 +0100

    Read the total unread from labels().get instead
    'threadsUnread' key in dict from 'labels().get'
    gives the total number of the specific label.
    This gives all unread emails and do not iterate
    through search results.
    
    Also added a note about labels.

--- launch.py
+++ launch.py
@@ -24,15 +24,9 @@ count_was = 0
 
 def update_count(count_was):
     gmail = discovery.build('gmail', 'v1', credentials=file.Storage(CREDENTIALS_PATH).get())
-    label_ids = ['INBOX', 'UNREAD']
+    label = 'INBOX' # Note: default labels are uppercase, such as the inbox or spam label
     user_id = 'me'
-    list = gmail.users().messages().list(userId=user_id, labelIds=label_ids).execute()
-    if 'resultSizeEstimate' in list:
-        count = list['resultSizeEstimate']
-        while 'nextPageToken' in list:
-            page_token = list['nextPageToken']
-            list = gmail.users().messages().list(userId=user_id, labelIds=label_ids, pageToken=page_token).execute()
-            count += list['resultSizeEstimate']
+    count = gmail.users().labels().get(userId=user_id, id=label).execute()['threadsUnread']
     if count > 0:
         print(unread_prefix + str(count), flush=True)
     else:
