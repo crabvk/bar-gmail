e617c1036753b467f5d342e54b0ec103c07e2317
Lautaro Pinilla <lpinilla@itba.edu.ar>
2020-06-29 21:20:58 -0300

    Working on multiple emails, auth.py adapted

--- auth.py
+++ auth.py
@@ -4,24 +4,52 @@ from pathlib import Path
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+import json
+import argparse
 
 SCOPE = 'https://www.googleapis.com/auth/gmail.labels'
 DIR = Path(__file__).resolve().parent
 CLIENT_SECRETS_PATH = Path(DIR, 'client_secrets.json')
 CREDENTIALS_PATH = Path(DIR, 'credentials.json')
 
-if Path(CREDENTIALS_PATH).is_file():
-    creds = Credentials.from_authorized_user_file(CREDENTIALS_PATH)
-    if creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        print('Credentials looks ok, try to remove credentials.json if something doesn\'t work')
-        exit()
-else:
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", "--addaccount", action='store_true')
+parser.add_argument("-c", "--color", default= "#ffffff")
+args = parser.parse_args()
+
+credentials = {
+    "count" : 0,
+    "creds" : [],
+}
+
+def add_cred():
     flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_PATH, scopes=[SCOPE])
     creds = flow.run_console()
+    credentials["count"] += 1
+    credentials["creds"].append( { "color": args.color, "cred" : json.loads(creds.to_json()) } )
+
+
+if Path(CREDENTIALS_PATH).is_file():
+    with open(CREDENTIALS_PATH, 'r') as f:
+            credentials = json.load(f)
+    if args.addaccount:
+        add_cred()
+    else:
+        #check all credentials for expired
+        refreshed = False
+        for i in range(0, credentials["count"]):
+            c = credentials["creds"][i]["cred"]
+            creds= Credentials.from_authorized_user_info(c)
+            if creds.expired and creds.refresh_token:
+                refreshed = True
+                creds.refresh(Request())
+        if refreshed:
+            print('Credentials looks ok, try to remove credentials.json if something doesn\'t work')
+            exit()
+else:
+    add_cred()
 
 # Save credentials
 with open(CREDENTIALS_PATH, 'w') as creds_file:
-    creds_file.write(creds.to_json())
+    json.dump(credentials, creds_file)
 print('Credentials successfully refreshed/created')
