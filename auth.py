e47dcc6a45cffe0fbaf0ae6b7597631353bb7599
crabvk <crabvk@protonmail.com>
2022-11-22 22:57:41 +0500

    Change auth flow method to local server, update deps

--- auth.py
+++ auth.py
@@ -5,7 +5,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-SCOPE = 'https://www.googleapis.com/auth/gmail.labels'
+SCOPES = 'https://www.googleapis.com/auth/gmail.labels'
 DIR = Path(__file__).resolve().parent
 CLIENT_SECRETS_PATH = Path(DIR, 'client_secrets.json')
 CREDENTIALS_PATH = Path(DIR, 'credentials.json')
@@ -15,13 +15,13 @@ if Path(CREDENTIALS_PATH).is_file():
     if creds.expired and creds.refresh_token:
         creds.refresh(Request())
     else:
-        print('Credentials looks ok, try to remove credentials.json if something doesn\'t work')
+        print('Credentials look OK, try to remove credentials.json if something doesn\'t work')
         exit()
 else:
-    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_PATH, scopes=[SCOPE])
-    creds = flow.run_console()
+    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_PATH, scopes=SCOPES)
+    creds = flow.run_local_server(open_browser=False)
 
 # Save credentials
 with open(CREDENTIALS_PATH, 'w') as creds_file:
     creds_file.write(creds.to_json())
-print('Credentials successfully refreshed/created')
+print('Credentials saved successfully')
